f51a9f1555f4cfc6da2c325b556466e2ef60f700
Akash <akashpatilap876716@gmail.com>
2023-02-13 15:51:38 +0530

    Chapter1

---
+++ Chapter1_Variables, Datatypes + Input & Output/Datatype.c
@@ -0,0 +1,11 @@
+// The datatypes "boolean" & "string", the concept of Objects & Classes etc does not exist in C Programming Language. (As C was the First Programming Language !)
+
+// Discovery : C ----> C++ ----> Java ----> Javascript 
+
+#include<stdio.h>
+
+int main()
+{
+  
+  return 0;
+}
