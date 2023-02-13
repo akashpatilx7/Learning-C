0b05baadaf7fd0e20928ecc284ce5d21b57b2d68
Akash <akashpatilap876716@gmail.com>
2023-02-13 15:24:17 +0530

    initial commit

---
+++ Hello.c
@@ -0,0 +1,16 @@
+/* The Naming of the C File can be done in any manner according to the User. 
+
+   Extension Of C Program Files : ".c"
+
+   Execution Command ----> For Mac/ Linux : ./a.out
+                           For Windows    : ./a.exe (As a .exe file (EXECUTABLE FILE) is created in Windows, BY DEFAULT)
+
+*/    
+
+#include<stdio.h>
+
+int main()
+{
+  printf("Hello World");
+  return 0;
+}
